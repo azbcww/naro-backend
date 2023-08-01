f7cd0bf26dbb5ab585f9bcd9b559b92415cb6549
azbcww <1121hatsuyuki@gmail.com>
2023-08-02 03:03:17 +0900

    プロジェクトのセットアップ

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	db *sqlx.DB
+)
 
 func main() {
-	fmt.Println("なろう講習会へようこそ！")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := mysql.Config{
+		User:      os.Getenv("DB_USERNAME"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
+		DBName:    os.Getenv("DB_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	_db, err := sqlx.Open("mysql", conf.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = _db
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityInfoHandler)
+	e.POST("/cities", postCityHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
