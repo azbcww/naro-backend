fcfc23e3cd24abc82d05c75b3d351d25e8b97a0f
azbcww <1121hatsuyuki@gmail.com>
2023-08-02 03:14:37 +0900

    account function

--- handler.go
+++ handler.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type City struct {
@@ -15,6 +16,11 @@ type City struct {
 	CountryCode sql.NullString `json:"countryCode,omitempty"  db:"CountryCode"`
 	District    sql.NullString `json:"district,omitempty"  db:"District"`
 	Population  sql.NullInt64  `json:"population,omitempty"  db:"Population"`
+}
+
+type LoginRequestBody struct {
+	Username string `json:"username,omitempty" form:"username"`
+	Password string `json:"password,omitempty" form:"password"`
 }
 
 func getCityInfoHandler(c echo.Context) error {
@@ -51,4 +57,46 @@ func postCityHandler(c echo.Context) error {
 	city.ID = int(id)
 
 	return c.JSON(http.StatusCreated, city)
+}
+
+func signUpHandler(c echo.Context) error {
+	// リクエストを受け取り、reqに格納する
+	req := LoginRequestBody{}
+	c.Bind(&req)
+
+	// バリデーションする(PasswordかUsernameが空文字列の場合は400 BadRequestを返す)
+	if req.Password == "" || req.Username == "" {
+		return c.String(http.StatusBadRequest, "Username or Password is empty")
+	}
+
+	// 登録しようとしているユーザーが既にデータベース内に存在するかチェック
+	var count int
+	err = db.Get(&count, "SELECT COUNT(*) FROM users WHERE Username=?", req.Username)
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	// 存在したら409 Conflictを返す
+	if count > 0 {
+		return c.String(http.StatusConflict, "Username is already used")
+	}
+
+	// パスワードをハッシュ化する
+	pw := req.Password + salt
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+	// ハッシュ化に失敗したら500 InternalServerErrorを返す
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	
+	// ユーザーを登録する
+	_, err = db.Exec("INSERT INTO users (Username, HashedPass) VALUES (?, ?)", req.Username, hashedPass)
+	// 登録に失敗したら500 InternalServerErrorを返す
+	if err != nil {
+		log.Println(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	// 登録に成功したら201 Createdを返す
+	return c.NoContent(http.StatusCreated)
 }
